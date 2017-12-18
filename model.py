bf387a2966bf07017225942008e7bbfc2bd52045
Eren OZDEK <eozdek@e1r2p2.42.fr>
2017-12-18 17:09:41 +0100

    essai4

--- model.py
+++ model.py
@@ -2,13 +2,16 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.nn import Parameter
 
-
+import torch.nn as nn
+from torch.autograd import Variable
 
 class BowModel(nn.Module):
     def __init__(self, emb_tensor):
         super(BowModel, self).__init__()
         n_embedding, dim = emb_tensor.size()
         self.embedding = nn.Embedding(n_embedding, dim, padding_idx=0)
+        self.embedding = nn.Embedding(n_embedding, dim, padding_idx=0)
+        # self.lstm = nn.LSTM(embedding_dim, hidden_dim)
         self.embedding.weight = Parameter(emb_tensor, requires_grad=False)
         self.out = nn.Linear(dim, 2)
 
@@ -20,4 +23,9 @@ class BowModel(nn.Module):
         # Here we take into account only the first word of the sentence
         # You should change it, e.g. by taking the average of the words of the sentence
         bow = embedded[:, 0]
-        return F.log_softmax(self.out(bow))
+        bow = embedded.mean(dim=1)
+        lstm_out, self.hidden = self.lstm(
+            embeds.view(input[1], 1, -1), self.hidden)
+        tag_space = self.hidden2tag(lstm_out.view(input[1], -1))
+        # tag_scores = F.log_softmax(tag_space)
+        return F.log_softmax(tag_space)
